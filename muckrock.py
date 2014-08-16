c1dbea8b97fff5aaf16267e40b14d39b518edfce
Thomas Levine <_@thomaslevine.com>
2014-08-16 10:11:30 +0000

    OrderedDict

--- muckrock.py
+++ muckrock.py
@@ -1,4 +1,5 @@
 import datetime
+from collections import OrderedDict
 import os
 
 import lxml.html
@@ -29,16 +30,16 @@ def parse_listings(html, downloaded_time):
     for tr in html.xpath('//table[@class="data-table"]/tr')[1:]:
         quiet = tr.xpath('td[position()=1]/em/text()')
         tags = [] if quiet == [] else quiet[0].split(', ')
-        yield {
-            'downloaded': downloaded_time,
-            'request': str(tr.xpath('td[position()=1]/a/@href')[0]),
-            'title': str(tr.xpath('td[position()=1]/a/text()')[0]),
-            'tags': list(map(str, tags)),
-            'user': str(tr.xpath('td[position()=2]/a/@href')[0]),
-            'status': str(tr.xpath('td[position()=3]/span/text()')[0]),
-            'jurisdiction': str(tr.xpath('td[position()=4]/a/@href')[0]),
-            'date': datetime.datetime.strptime(tr.xpath('td[position()=5]/text()')[0].strip(), '%b. %d, %Y').date(),
-        }
+        yield OrderedDict([
+            ('downloaded', downloaded_time),
+            ('request', str(tr.xpath('td[position()=1]/a/@href')[0])),
+            ('title', str(tr.xpath('td[position()=1]/a/text()')[0])),
+            ('tags', list(map(str, tags))),
+            ('user', str(tr.xpath('td[position()=2]/a/@href')[0])),
+            ('status', str(tr.xpath('td[position()=3]/span/text()')[0])),
+            ('jurisdiction', str(tr.xpath('td[position()=4]/a/@href')[0])),
+            ('date', datetime.datetime.strptime(tr.xpath('td[position()=5]/text()')[0].strip(), '%b. %d, %Y').date()),
+        ])
 
 def main():
     for listing in listings():
